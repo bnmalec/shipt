168e01dbbaf89e2b6bc180095c2ba7d0264efb82
Bonnie Malec <bnmalec@gmail.com>
2019-10-28 08:43:00 -0500

    Added some docs to tavern methods

--- test_starwars/test_helpers.py
+++ test_starwars/test_helpers.py
@@ -31,22 +31,36 @@ def search_response_for_item_by_name(response, key, name):
     assert name in results
 
 def load_json(response):
+    """ Load the json so we can work with it """
     dump = json.dumps(response.json())
     json_response = json.loads(dump)
     return json_response
 
 def check_starship_keys(response, **kwargs):
+    """ Loop through the values in the required element to look for the provided values
+    Parameters
+    ----------
+    kwargs: str
+        The keys we expect to find in the required set for the starship endpoint
+    """
     json_response = load_json(response)
     required = json_response['required']
     for k,v in kwargs.items():
         assert v in required
 
 def count_results(response, page):
+    """ Return a count of results from the json response """
     json_response = load_json(response)
     results = json_response['results']
     return Box({"count_on_page"+page: len(results)})
 
 def check_count_against_count_results(response, count1, count2, count3, count4):
+    """ Compares the stated count against the calculated count 
+    Parameters
+    ----------
+    count1, count2, count3, count4: int
+        The calculated counts from each respective page
+    """
     json_response = load_json(response)
     total_count = json_response['count']
     paginated_results_count = 0
